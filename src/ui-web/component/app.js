02c4c090e47b7f8bf214268ebbd2b651b73b0f98
Philip Lopez <Philip.Lopez@philiplopez.com>
2015-11-18 18:45:09 +1000

    cleaned up App component

--- src/ui-web/component/app.js
+++ src/ui-web/component/app.js
@@ -4,14 +4,10 @@ import h from "react-hyperscript";
 
 
 class App extends Component {
-    handleClick = () => {
-        this.refs.p.innerHTML = "OOPS!";
-    };
-
     render() {
         const {dispatch} = this.props;
         return h("div", [
-            h("p", {ref: "p", onClick: this.handleClick}, "Are YOU watching?")
+            h("p", {ref: "p"}, "Are YOU watching?")
         ]);
     }
 }
